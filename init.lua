1f31f4923bd0649d79a411c3ce8636acfd77c49b
Jeremy Novak <olepunchy@gmail.com>
2024-03-01 11:43:40 -0600

    upating config

--- init.lua
+++ init.lua
@@ -187,13 +187,10 @@ require("lazy").setup {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for neovim
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { "j-hui/fidget.nvim", opts = {} },
     },
     config = function()
