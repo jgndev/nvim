04e0c57f884f3e49298cfa0bd547753e96f81f25
Jeremy Novak <olepunchy@gmail.com>
2024-03-13 14:52:20 -0500

    updating config

--- init.lua
+++ init.lua
@@ -945,6 +945,10 @@ require("lazy").setup {
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
+
+  {
+    { "joerdav/templ.vim" },
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
