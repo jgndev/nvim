6ab396c9e1d1b039a87bba223f59ab5c8f7f2acd
Jeremy Novak <olepunchy@gmail.com>
2024-03-21 15:03:01 -0500

    updating config

--- lua/jgn/plugins/lsp-config.lua
+++ lua/jgn/plugins/lsp-config.lua
@@ -341,4 +341,13 @@ return {
       }
     end,
   },
+  {
+    require("lspconfig").terraformls.setup {},
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = { "*.tf", "*.tfvars" },
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    }),
+  },
 }
